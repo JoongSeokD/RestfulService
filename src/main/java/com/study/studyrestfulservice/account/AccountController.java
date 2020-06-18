d6af2fc7582919e274c2d37e4bf6600a797932c3
JoongSeokD <ljseokd@gmail.com>
2020-06-18 14:42:17 +0900

    HTTP Status Code 제어

--- src/main/java/com/study/studyrestfulservice/account/AccountController.java
+++ src/main/java/com/study/studyrestfulservice/account/AccountController.java
@@ -1,8 +1,11 @@
 package com.study.studyrestfulservice.account;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,8 +26,14 @@ public class AccountController {
     }
 
     @PostMapping("/users")
-    public void createAccount(@RequestBody Account account){
+    public ResponseEntity<Account> createAccount(@RequestBody Account account){
         Account saveAccount = accountDaoService.save(account);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveAccount.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
 
