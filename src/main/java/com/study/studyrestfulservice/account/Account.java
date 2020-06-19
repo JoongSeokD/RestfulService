f4df61c44efbca567038b6623e411df8e178348e
JoongSeokD <ljseokd@gmail.com>
2020-06-19 11:08:28 +0900

    Swagger Documentation 구현 방법

--- src/main/java/com/study/studyrestfulservice/account/Account.java
+++ src/main/java/com/study/studyrestfulservice/account/Account.java
@@ -3,6 +3,8 @@ package com.study.studyrestfulservice.account;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +18,22 @@ import java.time.LocalDateTime;
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class Account {
     private Long id;
 
     @Size(min = 2, message = "Name은 2글자 이상 입력해 주세요.")
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
+
     @Past
+    @ApiModelProperty(notes = "사용자의 등록일을 입력해 주세요.")
     private LocalDateTime joinDate;
 
 //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자의 비밀번호를 입력해 주세요.")
     private String password;
 //    @JsonIgnore
+    @ApiModelProperty(notes = "사용자의 주민등록번호를 입력해 주세요.")
     private String ssn;
 }
