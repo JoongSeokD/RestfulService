be61889ecd1336ef4aee29932d99bb536053525c
JoongSeokD <ljseokd@gmail.com>
2020-06-22 13:09:22 +0900

    Spring Data JPA를 이용한 Entity 설정과 초기 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into account values(1, sysdate(), 'account1', 'test1', '701010-1111111');
+insert into account values(2, sysdate(), 'account2', 'test2', '801010-2222222');
+insert into account values(3, sysdate(), 'account3', 'test3', '901010-3333333');
